72851e77093c0b15cc9097a8ea1bd3c8064ae434
Kyle Szalai <kyleszalai@gmail.com>
2019-06-02 22:18:34 -0400

    Adding React version of site
    Adding React version of site from kszalai-website_react build

---
+++ precache-manifest.e879d54fba7fe0b09d69553f5aef9a48.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33336fee242101a7e0d13aabe2b525dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2a2f8514ef1df884ea0",
+    "url": "/static/css/2.2b8ce1c4.chunk.css"
+  },
+  {
+    "revision": "1a71fcd3421d6d0acac3",
+    "url": "/static/css/main.af349916.chunk.css"
+  },
+  {
+    "revision": "e2a2f8514ef1df884ea0",
+    "url": "/static/js/2.3d3e5e3a.chunk.js"
+  },
+  {
+    "revision": "1a71fcd3421d6d0acac3",
+    "url": "/static/js/main.ff9612a0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0f964a72f8fbdf9c8d4e0122b8effb40",
+    "url": "/static/media/devicon.0f964a72.woff"
+  },
+  {
+    "revision": "29f80b00a7b4641183f96f08be374697",
+    "url": "/static/media/devicon.29f80b00.ttf"
+  },
+  {
+    "revision": "681fa39a79c22f0035a0720e2b2bda3b",
+    "url": "/static/media/devicon.681fa39a.eot"
+  },
+  {
+    "revision": "74e6e6c3dcfca56767adabe83a510f2e",
+    "url": "/static/media/devicon.74e6e6c3.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "1fa5191de946b46c0717536cc77e5981",
+    "url": "/static/media/profile_pic.1fa5191d.jpg"
+  }
+]);
